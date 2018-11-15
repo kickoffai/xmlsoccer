d8aa1bd9aae9fab24a3490d6907edbe63fc06452
Andrei Kozlov <luberlinder@gmail.com>
2018-11-15 23:01:20 +0100

    Added some methods

--- xmlsoccer/client.py
+++ xmlsoccer/client.py
@@ -1,3 +1,4 @@
+from datetime import date
 import requests
 
 from .parsers import *
@@ -19,11 +20,31 @@ class Client:
         res = self._get("GetAllOddsByFixtureMatchId", fixtureMatch_Id=fid)
         return parse_odds(res)
 
-    def get_fixtures(self, *, league, season):
+    def get_fixtures(self, *, league, season=''):
         """Get matches given a league and a season."""
         res = self._get(
                 "GetFixturesByLeagueAndSeason",
                 seasonDateString=season,
+                league=league)
+        return parse_fixtures(res)
+        
+    def get_fixture(self, *, fid):
+        """Get match by ID."""
+        res = self._get(
+                "GetFixtureMatchByID",
+                id=fid)
+        return parse_fixtures(res)
+        
+    def get_fixtures_by_date_interval(self, *, league, start=None, end=None):
+        """Get matches given a league and a start/end date interval in ISO format yyyy-mm-dd."""
+        if not start:
+            start = date.today().isoformat()
+        if not end:
+            end = date(date.today().year, 12, 31).isoformat()
+        res = self._get(
+                "GetFixturesByDateIntervalAndLeague",
+                startDateString=start,
+                endDateString=end,
                 league=league)
         return parse_fixtures(res)
 
